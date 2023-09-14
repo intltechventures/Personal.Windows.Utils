c69dae32209ae06ffb7a77833e63d541004b9f96
Kelvin D. Meeks <kmeeks@intltechventures.com>
2023-09-13 20:01:08 -0700

    added scan for D:\, added ECHO comments, restructured to do sfc scan first

--- shazam_integrity_check.bat
+++ shazam_integrity_check.bat
@@ -1,4 +1,19 @@
-chkdsk c: /scan /perf /V  
-pause
-
+ECHO .
+ECHO Running sfc scan...
+ECHO .
 sfc /scannow
+
+pause
+ECHO .  
+ECHO Running chkdsk on C:\
+ECHO .
+chkdsk c: /scan /perf /V
+
+pause
+ECHO .
+ECHO Running chkdsk on D:\
+ECHO .
+chkdsk d: /scan /perf /V
+
+
+
