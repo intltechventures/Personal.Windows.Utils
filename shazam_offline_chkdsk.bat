cdf123d69b2ad3f42c8cb325a624280d79bf0d0b
Kelvin D. Meeks <kmeeks@intltechventures.com>
2023-09-16 12:24:34 -0700

    initial commit

---
+++ shazam_offline_chkdsk.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+cls
+ECHO shazam_offline_chkdsk.bat utility script - prepared by Kelvin D. Meeks
+ECHO Purpose: Run an offline chkdsk command to repair a drive 
+ECHO https://github.com/intltechventures/Personal.Windows.Utils/blob/master/shazam.bat
+ECHO Version: 1.0.0, 2023-09-16
+ECHO.
+ECHO.
+ECHO =======================================================================================
+ECHO Scan and Fix any Disk Errors
+REM OFF
+REM https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/chkdsk?tabs=event-viewer
+REM ON 
+ECHO 	Command: chkdsk /scan /perf /sdcleanup
+chkdsk c: /scan /perf /sdcleanup
